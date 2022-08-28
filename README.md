3aad83bac91dc2a24ebbb4dca38f0bf572e67bb2
madhuj31 <95279507+madhuj31@users.noreply.github.com>
2022-08-28 16:53:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# smaple-project
+# madhuj31
---
+++ regressionTest.bat
@@ -0,0 +1,8 @@
+echo "*** Welcome to Batch Exeuction ***"
+d:
+cd F:\ITA_Nov_21\Eclipse_WS\actitime
+set path=%path%;C:\Users\Madhu\.m2\repository\org\apache\maven
+set classpath=D:\ITA\ITAutomation_Nov_21_Batch\Eclipse_WS\actitime\target\*;.
+mvn test
+
+
